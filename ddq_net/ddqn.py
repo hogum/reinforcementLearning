3c74cfe36ca7ec5f6b41a784a80d5fe0ca5b506c
mugoh <mugoh.ks@gmail.com>
2019-07-26 10:34:59 +0300

    (dddqn): play trained agent

--- ddq_net/ddqn.py
+++ ddq_net/ddqn.py
@@ -77,7 +77,7 @@ def stack_frames(state, stacked_frames=None, new_episode=False):
 
 
 @dataclass
-class DoomDDpqN:
+class DoomDDdqN:
     """
         Deep Q Network model for doom.
 
@@ -252,7 +252,7 @@ class DoomDDpqN:
 
         self.game, self.actions_choice = create_env()
         self.game.new_episode()
-        state = game.get_state().screen_buffer
+        state = self.game.get_state().screen_buffer
         state, stacked_frames = stack_frames(state, new_episode=True)
 
         for episode in range(episodes):
@@ -268,7 +268,7 @@ class DoomDDpqN:
                 state = self.game.get_state().screen_buffer
                 state, stacked_frames = stack_frames(state, new_episode=True)
             else:
-                next_state = game.get_state().screen_buffer
+                next_state = self.game.get_state().screen_buffer
                 next_state, stacked_frames = stack_frames(
                     next_state, stacked_frames)
                 self.memory + [state, action, reward, next_state, done]
@@ -297,8 +297,8 @@ class DoomDDpqN:
 
         if explore_prob > explore_exploit_tradeoff:
             # Explore
-            action = self.possible_actions[np.random.choice(
-                self.possible_actions.shape[0], size=1)][0]
+            action = self.actions_choice[np.random.choice(
+                self.actions_choice.shape[0], size=1)][0]
         else:
             # Exploit -> Estimate Q values state
             Qs = sess.run(
@@ -307,19 +307,22 @@ class DoomDDpqN:
                     self.inputs: state.reshape((1, *state.shape))})
             # Best action
             choice = np.argmax(Qs)
-            action = self.possible_actions[int(choice)]
+            action = self.actions_choice[int(choice)]
         return list(action), explore_prob
 
     def update_target_graph(self):
         """
             Copies parameters of the DQN to the target network
         """
-        from_vars = tf.collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'DQNet')
-        to_vars = tf.collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'TargetNet')
+        from_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, 'DQNet')
+        to_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, 'TargetNet')
 
         up_holder = [to_vars.assign(from_vars)
                      for from_vars, to_vars in zip(from_vars, to_vars)]
         return up_holder
+
     def train(self, episodes=50, batch_size=64, max_steps=300, training=True):
         """
             Trains the model
@@ -329,4 +332,218 @@ class DoomDDpqN:
                 sess.run(tf.global_variables_initializer())
                 decay_step = 0
                 tau = 0
+                loss = ''
                 self.game.init()
+
+                sess.run(self.update_target_graph())
+
+                for episode in range(episodes):
+                    step = 0
+                    episode_rewards = []
+                    self.game.new_episode()
+                    state = self.game.get_state().screen_buffer
+                    state, stacked_frames = stack_frames(
+                        state, new_episode=True)
+
+                    while step <= max_steps:
+                        step += 1
+                        tau += 1
+                        decay_step += 1
+
+                        action, explore_prob = self.predict_action(
+                            sess, state, decay_step)
+                        reward = self.game.make_action(action)
+                        episode_rewards += [reward]
+                        done = self.game.is_episode_finished()
+
+                        if done:
+                            next_state = np.zeros((120, 140),
+                                                  # resolution,
+                                                  dtype=np.int)
+                            next_state, stacked_frames = stack_frames(
+                                next_state, stacked_frames)
+                            step = max_steps
+                            total_reward = np.sum(episode_rewards)
+
+                            print(f'Episode {episode}' +
+                                  f'Total reward: {total_reward}' +
+                                  f'loss: {loss}' +
+                                  f'Explore prob: {explore_prob}'
+                                  )
+                            exp = state, action, reward, next_state, done
+                            self.memory.add(exp)
+                        elif not done:
+                            next_state = self.game.get_state().screen_buffer
+                            next_state, stacked_frames = stack_frames(
+                                next_state, stacked_frames)
+                            self.memory + (state, action, reward,
+                                           next_state, done)
+                            state = next_state
+                        acc, loss, abs_err = self._learn(
+                            sess, episode, batch_size)
+
+                        if tau > self.max_tau:
+                            sess.run(self.update_target_graph())
+                            tau = 0
+
+                        self.save(self, sess, episode)
+
+    def _learn(self, sess, episode, batch_size):
+        """
+            Uses experiences stored in memory to get
+            target Q values
+        """
+        mini_batches, tree_index = self.sample_experiences(batch_size)
+        targets = self.get_target_Qs(sess, mini_batches)
+        acc, loss, abs_errs = self.find_loss(sess, targets, mini_batches)
+
+        self.memory.update_priorities(tree_index, abs_errs)
+        mini_batches.update({'targets': targets})
+        self.summarize(sess, episode, mini_batches)
+
+        return acc, loss, abs_errs
+
+    def get_target_Qs(self, sess, mini_batch):
+        """
+            Sets the target_ Q as r for episodes ending at s + 1
+            else, at r + gamma * max[Q(s',a')]
+        """
+        q_next_state = sess.run(self.output,
+                                feed_dict={
+                                    self.inputs: mini_batch.get('next_states')
+                                })
+        q_target_ns = sess.run(
+            target_net.output,
+            feed_dict={
+                target_net.inputs: mini_batch.get('next_states')})
+
+        target_Qs = []
+
+        for i in range(mini_batch.get('batch_len')):
+            terminal = mini_batch.get('dones')[i]
+            action = np.argmax(q_next_state)[i]
+            rewards = mini_batch.get('rewards')[i]
+
+            if terminal:
+                target_Qs.append(rewards)
+            else:
+                target_Qs.append(rewards + self.gamma * q_target_ns[i][action])
+
+        targets_mb = [m_b for m_b in target_Qs]
+
+        return targets_mb
+
+    def find_loss(self, sess, targets, mini_batches):
+        """
+            Finds difference between Q and targets
+        """
+        acc, loss, err = sess.run(
+            [self.optimizer, self.loss, self.abs_errors],
+            feed_dict={self.inputs: mini_batches.get('states'),
+                       self.target_Q: targets,
+                       self.actions: mini_batches.get('actions'),
+                       self.ISweights: mini_batches.get(
+                'ISweights')
+            })
+        return acc, loss, err
+
+    def sample_experiences(self, batch_size):
+        """
+            Samples experience mini batches from memory
+        """
+        tree_index, batch, IS_weights = self.memory.sample(batch_size)
+        states = self.__from_memory(batch, key=0, min_dims=3)
+        actions = self.__from_memory(batch, 1)
+        rewards = self.__from_memory(batch, 2)
+        next_states = self.__from_memory(batch, 3, 3)
+        dones = self.__from_memory(batch, 4)
+
+        return {
+            'states': states,
+            'actions': actions,
+            'rewards': rewards,
+            'next_states': next_states,
+            'dones': dones,
+            'batch_len': len(batch),
+            'ISweights': IS_weights
+        }, tree_index
+
+    def __from_memory(self, batch, key, min_dims=0):
+        """
+            Gives states, actions, rewards, as mini
+            batches from a memory sample
+        """
+        f_key = 0
+        m_b = np.array(batch[f_key][key], ndmin=min_dims)
+
+        return m_b
+
+    def summarize(self, sess, episode, batches):
+        """
+            Writes tf summaries
+        """
+        summary = sess.run(
+            self.writer_op,
+            feed_dict={self.inputs: batches.get('states'),
+                       self.target_Q: batches.get('targets'),
+                       self.actions: batches.get('actions'),
+                       self.ISweights: batches.get('ISweights')})
+        self.writer.add_summary(summary, episode)
+        self.writer.flush()
+
+    def save(self, sess, episode, interval=5):
+        """
+            Updates and saves the model
+        """
+        if not episode % interval:
+            self.saver.save(sess, './models/dddqn.ckpt')
+
+    def play(self, episodes=25):
+        """
+           Plays the trained agent
+        """
+        with tf.compat.v1.Session() as sess:
+            game, actions_choice = create_env()
+            game.load_config('deadly_corridor_testing.cfg')
+            game.set_doom_scenario_path('deadly_corridor.wad')
+            eps = .01
+
+            self.saver.restore(sess, './models/dddqn.ckpt')
+            game.init()
+
+            for i in range(episodes):
+                game.new_episode()
+                state = game.get_state().screen_buffer
+                state, stacked_frames = stack_frames(state, new_episode=True)
+
+                while not game.is_episode_finished():
+                    tradeoff = np.random.randn()
+
+                    if tradeoff > eps:
+
+                        action = actions_choice[np.random.choice(
+                            actions_choice.shape[0], size=1)][0]
+                    else:
+                        # Exploit -> Estimate Q values state
+                        Qs = sess.run(
+                            self.output,
+                            feed_dict={
+                                self.inputs: state.reshape((1, *state.shape))})
+                        # Best action
+                        choice = np.argmax(Qs)
+                        action = self.actions_choice[int(choice)]
+                    game.make_action(list(action))
+                    done = game.is_episode_finished()
+
+                    if not done:
+                        next_state = game.get_state().screen_buffer
+                        next_state, stacked_frames = stack_frames(
+                            next_state, stacked_frames)
+                        state = next_state
+                    else:
+                        break
+                print(f'Score: {game.get_total_reward()}')
+            game.close()
+
+
+target_net = DoomDDdqN()
