09d35c12c5d2ecb1adba8f96e2ce54e62134f34c
mugoh <mugoh.ks@gmail.com>
2019-08-01 18:55:58 +0300

    calculate loss and update params

--- a2c/a2_c.py
+++ a2c/a2_c.py
@@ -6,6 +6,9 @@ from typing import Any
 
 import tensorflow as tf
 
+from utils import mse
+
+
 @dataclass
 class Model:
     """
@@ -22,20 +25,54 @@ class Model:
 
     def __post_init__(self):
         self.build_model()
+
     def build_model(self):
         """
             Creates the model
         """
         sess = tf.get_default_session()
         actions = tf.compat.v1.placeholder(tf.int, [None], name='actions')
-        advantages = tf.compat.v1.placeholder(tf.float32, [None], name='advantages')
+        advantages = tf.compat.v1.placeholder(
+            tf.float32, [None], name='advantages')
         rewards = tf.compat.v1.placeholder(tf.float32, (None), name='rewards')
         lr = tf.compat.v1.placeholder(tf.float32, name='learning_rate')
 
-        step_model = policy(sess, obsv_space, action_space, n_envs, 1, reuse=False)
-        train_model = policy(sess, obsv_space, action_space, n_envs*n_steps, n_steps, reuse=True)
+        step_model = policy(sess, obsv_space, action_space,
+                            n_envs, 1, reuse=False)
+        train_model = policy(sess, obsv_space, action_space,
+                             n_envs*n_steps, n_steps, reuse=True)
 
-        # Loss = Policy gradient loss - entropy * entropy_coeff + value_coeff  * value los
+        # Loss = (Policy gradient loss - entropy) *
+        #   (entropy_coeff + value_coeff  * value loss)
+
+        # Output - log(pi)
+        neglogpac = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=train_model.pi,
+            labels=actions)
+
+        # 1/n * sum A(si,ai) * -logpi(si|ai)
+        pg_loss = tf.reduce_mean(advantages * neglogpac)
+
+        # 1/2 * sum[R - V(s)] ** 2
+        vf_loss = tf.reduce_mean(mse(tf.squeeze(train_model.vf), rewards))
+
+        # Limits premature convergence to subotimal policy
+        entropy = tf.reduce_mean(train_model.pd.entropy())
+        loss = pg_loss - entropy * ent_coef + vf_loss * vf_coef
+
+        # Update parameters using loss
+        params = find_trainable_vars('model')
+        grads = tf.gradients(loss, params)
+
+        if max_grad_norm is not None:
+            # clip grads [Noramalize]
+            grads, grad_norm = tf.clip_by_global_norm(grads, max_grad_norm)
+        grads = list(zip(grads, params)
+
+        optimizer=tf.train.RMSPropOptimizer(learning_rate=lr,
+            decay_rate=.99,
+            epsilon=1e-5)
+        train_=optimizer.apply_gradients(grads)
 
     def save(self):
         """
