ee66e8ed521a12a5601c103dee7bb45b084a2f4c
mugoh <mugoh.ks@gmail.com>
2019-08-10 11:17:29 +0300

    define inputs

--- curiosity_driven/arch.py
+++ curiosity_driven/arch.py
@@ -22,3 +22,27 @@ class Curiosity:
     def __post_init__(self):
         self.memory = np.zeros((self.mem_size, self.n_states * 2 + 2))
         self.build_model()
+    def build_net(self):
+        """
+            Creates the NN model
+        """
+        self.states = tf.compat.v1.placeholder(tf.float32, (None, self.n_states), name='states')
+        self.actions = tf.compat.v1.placeholder(tf.int32, (None,), name='actions')
+        self.rewards = tf.compat.v1.placeholder(tf.float32, (None,), name='external_rewards')
+        self.next_states = tf.compat.v1.placeholder(tf.float32, (None, self.n_states), name='next_states')
+
+        self.build_dynamics_net()
+
+    def build_dynamics_net(self):
+        """
+            Predicts next state
+            : loss error between actual state and predicted state
+        """
+        with tf.compat.v1.variable_scope('dynamic_net'):
+            actions = tf.expand_dims(tf.cast(self.actions, dtype=tf.float32, name='actions_float'), name='actions_2d',axis=1)
+            stat_act = tf.concat((self.states, actions), axis=1, name='state_actions')
+            encoded_states = self.next_states
+
+
+
+
